0c62a3ff9afe5bd1e0cd31aee65c83aca38b20ae
Duda Ferreira <eduarda_26_duda@hotmail.com>
2022-02-12 19:15:50 -0300

    git commit -m "✨feat: teste dsgdg -   see: http://localhost:8080/swagger-ui.html#/commits-api"

---
+++ src/main/java/br/com/eduardacf/config/swagger/Swagger2Config.java
@@ -0,0 +1,61 @@
+package br.com.eduardacf.config.swagger;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@EnableAutoConfiguration
+public class Swagger2Config {
+
+    @Value("${enable.swagger2}")
+    boolean enableSwagger2;
+
+    @Bean
+    public Docket swagger() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .enable(enableSwagger2)
+                .apiInfo(apiInfo())
+                .select()
+                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("br.com.eduardacf"))
+                .build();
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Sistema de geração de commits de forma semântica")
+                .version("v1.0")
+                .contact(new Contact("Eduarda Ferreira", "https://www.linkedin.com/in/eduarda-ferreira/", "eduarda.cferreira10@gmail.com"))
+                .build();
+    }
+
+
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("br.com.eduardacf"))
+//                .build()
+//                .apiInfo(apiInfo());
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("Sistema de geração de commits de forma semântica")
+//                .version("1.0.0")
+//                .contact(new Contact("Eduarda Ferreira", "https://www.linkedin.com/in/eduarda-ferreira/", "eduarda.cferreira10@gmail.com"))
+//                .build();
+//    }
+}
+

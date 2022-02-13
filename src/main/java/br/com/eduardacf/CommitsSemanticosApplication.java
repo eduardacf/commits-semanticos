37aed3ec0cf641331c921fd01b9d5b4545cd1d1a
Duda Ferreira <eduarda_26_duda@hotmail.com>
2022-02-13 13:07:30 -0300

    git commit -m "✨feat: teste dsgdg -   see: http://localhost:8080/swagger-ui.html#/commits-api"

--- src/main/java/br/com/eduardacf/CommitsSemanticosApplication.java
+++ src/main/java/br/com/eduardacf/CommitsSemanticosApplication.java
@@ -2,46 +2,14 @@ package br.com.eduardacf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class CommitsSemanticosApplication {
 
     public static void main(String args[]) {
         SpringApplication.run(CommitsSemanticosApplication.class, args);
     }
-
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-
-                registry.addMapping("/v1/**")
-                        .allowedOrigins("http://localhost:8080")
-                        .allowedMethods("*");
-
-                // to heroku allow connections from netlify
-                registry.addMapping("/v1/**")
-                        .allowedMethods("*");
-
-                registry.addMapping("/swagger-ui/**")
-                        .allowedOrigins("*");
-
-                registry.addMapping("/api/v2/api-docs")
-                        .allowedOrigins("*");
-
-            }
-
-        };
-
-
-    }
-
 }
+
+
+
--- src/main/java/br/com/eduardacf/config/swagger/SwaggerConfig.java
+++ src/main/java/br/com/eduardacf/config/swagger/SwaggerConfig.java
@@ -20,6 +20,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
+
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }

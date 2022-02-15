6297aac7ccf7dc95dd512871e10e213ff9d57c56
Duda Ferreira <eduarda_26_duda@hotmail.com>
2022-02-14 21:59:21 -0300

    git commit -m "✨feat: teste dsgdg -   see: http://localhost:8080/swagger-ui.html#/commits-api"

--- src/main/java/br/com/eduardacf/config/swagger/SwaggerConfig.java
+++ src/main/java/br/com/eduardacf/config/swagger/SwaggerConfig.java
@@ -24,6 +24,9 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/WEB-INF/resources/");
     }
 
     @Bean
